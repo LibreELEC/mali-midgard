ec046ed88d1b3dd70307963d3ae474f56029ae71
Neil Armstrong <narmstrong@baylibre.com>
2018-12-10 14:05:57 +0100

    Add T820 compatible

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -4069,6 +4069,7 @@ static const struct dev_pm_ops kbase_pm_ops = {
 static const struct of_device_id kbase_dt_ids[] = {
 	{ .compatible = "arm,malit6xx" },
 	{ .compatible = "arm,mali-t860" },
+	{ .compatible = "arm,mali-t820" },
 	{ .compatible = "arm,mali-midgard" },
 	{ /* sentinel */ }
 };
