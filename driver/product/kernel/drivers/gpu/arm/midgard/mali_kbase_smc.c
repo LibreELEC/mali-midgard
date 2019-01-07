2798d8ecc653a3ae197e65b26e1b7288e92fc695
Jonas Karlman <jonas@kwiboo.se>
2019-01-07 22:37:58 +0100

    v4.20 build fix

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_smc.c
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_smc.c
@@ -27,6 +27,10 @@
 
 #include <linux/compiler.h>
 
+#ifndef __asmeq
+#define __asmeq(x, y)  ".ifnc " x "," y " ; .err ; .endif\n\t"
+#endif
+
 static noinline u64 invoke_smc_fid(u64 function_id,
 		u64 arg0, u64 arg1, u64 arg2)
 {
